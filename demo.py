7e67ca9f39cb03b5d49a1f790009d64f6e063ade
David Stafford <dstaff@kalexland.com>
2021-09-16 16:25:51 -0700

    fix module and add demo

---
+++ demo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import geometry
+import sys
+
+
+def main():
+    ct = geometry.CoordinateTree()
+    ct.load_from_file(sys.argv[1])
+
+    print(ct.get_projection_matrix("system3", "system1"))
+
+    f = open("3.pov", "w");
+    f.write(ct.to_pov_ray())
+
+main()

c409c37e7a9e766c5c0028af46cdc386c66e1d31
David Stafford <dstaff@kalexland.com>
2021-09-16 18:47:23 -0700

    Fix formatting.

--- geometry/__init__.py
+++ geometry/__init__.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-# fix a*m v. m*a bug
-# fix type hints
-# errors for unreachable nodes
 
-
-import numpy;
-import sys;
+import numpy
+import sys
 import json
 import math
 
-def deg2rad(x:float):
+
+def deg2rad(x: float):
     """ Convert from degreese to radians"""
     return math.pi*x/180
 
@@ -34,20 +31,20 @@ class CoordinateSystem:
         """ Compute a numpy matrix representig the active translation of coordiantes
         in this coordinate system to coordinates in the parent coordinate system
         """
-        s1=math.sin(deg2rad(self.angle_x));
-        s2=math.sin(deg2rad(self.angle_y));
-        s3=math.sin(deg2rad(self.angle_z));
-        c1=math.cos(deg2rad(self.angle_x));
-        c2=math.cos(deg2rad(self.angle_y));
-        c3=math.cos(deg2rad(self.angle_z));
+        s1 = math.sin(deg2rad(self.angle_x))
+        s2 = math.sin(deg2rad(self.angle_y))
+        s3 = math.sin(deg2rad(self.angle_z))
+        c1 = math.cos(deg2rad(self.angle_x))
+        c2 = math.cos(deg2rad(self.angle_y))
+        c3 = math.cos(deg2rad(self.angle_z))
 
         v1 = [c2*c3, -s2, -c2*s3, self.offset_x]
         v2 = [s1*s3 + c1*c3*s2, c1*c2, c1*s2*s3-c3*s1, self.offset_y]
         v3 = [c3*s1*s2 - c1*s3, c2*s1, c1*c3 + s1*s2*s3, self.offset_z]
-        v4 = [0,0,0,1]
+        v4 = [0, 0, 0, 1]
 
-        return numpy.matrix([v1,v2,v3,v4])
-    
+        return numpy.matrix([v1, v2, v3, v4])
+
     def get_passive_matrix(self):
         """ Compute a numpy matrix representing the passive translation of
         coordinates in the parent coordinate system into this coordinate 
@@ -68,13 +65,13 @@ class CoordinateSystem:
                     order of rotation is X, Y, Z.
         """
         self.offset_x, \
-        self.offset_y, \
-        self.offset_z, \
-        self.angle_x,  \
-        self.angle_y,  \
-        self.angle_z = vec
+            self.offset_y, \
+            self.offset_z, \
+            self.angle_x,  \
+            self.angle_y,  \
+            self.angle_z = vec
 
-        self.name=name
+        self.name = name
         self.parent_name = parent
 
 
@@ -84,10 +81,10 @@ class CoordinateTreeNode:
     children: 'CoordinateTreeNode'
     level: int
 
-    def __init__(self, cs:CoordinateSystem, parent:'CoordinateTreeNode', level:int):
-        self.cs=cs
-        self.parent=parent
-        self.level=level
+    def __init__(self, cs: CoordinateSystem, parent: 'CoordinateTreeNode', level: int):
+        self.cs = cs
+        self.parent = parent
+        self.level = level
         self.children = []
 
 
@@ -96,12 +93,11 @@ class CoordinateTree:
     from one coordinate system to another
     """
 
-
     root: CoordinateSystem      # Special Root coordinate system
     rootT: CoordinateTreeNode   # Tree Node fro the root coordinate system
     NameToCS = {}               # Mapping of coordinate system names
-    
-    def get_projection_matrix(self, frm:str, to:str):
+
+    def get_projection_matrix(self, frm: str, to: str):
         """Compute a matrix that translates points in the coordinate
         system of "frm" to the coordinate system "to".
           to   -  the name of the new coordinate system.
@@ -111,7 +107,7 @@ class CoordinateTree:
         If you multiply it by a vector [[X],[Y],[Z],[1]] you will
         transform it from the 'to' system to the 'frm' system
         """
-        
+
         f = self.NameToCS[frm]
         t = self.NameToCS[to]
 
@@ -121,37 +117,38 @@ class CoordinateTree:
         # for each coordinate system we pass through
 
         while (not (t is f)):
-            if t.level>=f.level:
-                m = m * t.cs.get_active_matrix();
-                t=t.parent
-            elif f.level>=t.level:
+            if t.level >= f.level:
+                m = m * t.cs.get_active_matrix()
+                t = t.parent
+            elif f.level >= t.level:
                 #m = m * f.cs.get_passive_matrix();
-                m =  f.cs.get_passive_matrix() * m;
-                f=f.parent
+                m = f.cs.get_passive_matrix() * m
+                f = f.parent
         return m
 
-    def load_from_file(self, fn:str):
+    def load_from_file(self, fn: str):
         """Load coordinate systems from a file
           - fn a JSON file containing coordinate system information
         """
 
         # BUG -- failed open
-        fp = open(fn);
+        fp = open(fn)
 
         # Bug bad JSON
         j = json.load(fp)
 
         for entry in j:
             # BUG missing or incorrect fields
-            newcs = CoordinateSystem(entry["name"], entry["parent"], entry["relative"])
-            
+            newcs = CoordinateSystem(
+                entry["name"], entry["parent"], entry["relative"])
+
             self.NameToCS[newcs.name] = CoordinateTreeNode(newcs, None, 0)
 
-        # Build a root node. 
+        # Build a root node.
         # BUG -- we assume everything is reachable from root
-        self.root = CoordinateSystem("root", None, [0,0,0,0,0,0]);
+        self.root = CoordinateSystem("root", None, [0, 0, 0, 0, 0, 0])
         self.rootT = CoordinateTreeNode(self.root, None, 0)
-        self.NameToCS["root"]=self.rootT
+        self.NameToCS["root"] = self.rootT
 
         # fill out the parent and child relationships for each node
         for csts in self.NameToCS:
@@ -159,13 +156,12 @@ class CoordinateTree:
             if (cst.cs.parent_name != None):
                 cst.parent = self.NameToCS[cst.cs.parent_name]
                 cst.parent.children.append(cst)
-        
+
         # Fill out the level for each node
         self.resolve_levels(self.rootT, 0)
 
-            
-    def resolve_levels(self, cur:CoordinateTreeNode, level:int):
-        cur.level=level
+    def resolve_levels(self, cur: CoordinateTreeNode, level: int):
+        cur.level = level
         for n in cur.children:
             self.resolve_levels(n, level+1)
 
@@ -173,29 +169,29 @@ class CoordinateTree:
         """ Write out a snippet of pov-ray code that shows every coordinate system"""
 
         output_str = ""
-        xp_vec = numpy.matrix([[1],[0],[0],[1]]);
-        yp_vec = numpy.matrix([[0],[1],[0],[1]]);
-        zp_vec = numpy.matrix([[0],[0],[1],[1]]);
+        xp_vec = numpy.matrix([[1], [0], [0], [1]])
+        yp_vec = numpy.matrix([[0], [1], [0], [1]])
+        zp_vec = numpy.matrix([[0], [0], [1], [1]])
 
-        xn_vec = numpy.matrix([[-1],[0],[0],[1]]);
-        yn_vec = numpy.matrix([[0],[-1],[0],[1]]);
-        zn_vec = numpy.matrix([[0],[0],[-1],[1]]);
+        xn_vec = numpy.matrix([[-1], [0], [0], [1]])
+        yn_vec = numpy.matrix([[0], [-1], [0], [1]])
+        zn_vec = numpy.matrix([[0], [0], [-1], [1]])
 
         for csts in self.NameToCS:
-            tx = self.get_projection_matrix(r, csts);
-            xp_proj = tx*xp_vec;
-            yp_proj = tx*yp_vec;
-            zp_proj = tx*zp_vec;
-            xn_proj = tx*xn_vec;
-            yn_proj = tx*yn_vec;
-            zn_proj = tx*zn_vec;
-            
-            output_str += self.povray_cylinder(xp_proj, xn_proj, "Red");
-            output_str += self.povray_cylinder(yp_proj, yn_proj, "Green");
-            output_str += self.povray_cylinder(zp_proj, zn_proj, "Blue");
+            tx = self.get_projection_matrix(r, csts)
+            xp_proj = tx*xp_vec
+            yp_proj = tx*yp_vec
+            zp_proj = tx*zp_vec
+            xn_proj = tx*xn_vec
+            yn_proj = tx*yn_vec
+            zn_proj = tx*zn_vec
+
+            output_str += self.povray_cylinder(xp_proj, xn_proj, "Red")
+            output_str += self.povray_cylinder(yp_proj, yn_proj, "Green")
+            output_str += self.povray_cylinder(zp_proj, zn_proj, "Blue")
             output_str +=\
                 'text {{ internal 1 "{0}" 0.1, 0  scale .2 pigment {{Yellow}} translate <{1}, {2}, {3}>}}\n'\
-                .format(csts,xp_proj[0,0], xp_proj[1,0]+.1, xp_proj[2,0]) 
+                .format(csts, xp_proj[0, 0], xp_proj[1, 0]+.1, xp_proj[2, 0])
         return self.pov_ray_preamble() + output_str
 
     def pov_ray_preamble(self):
@@ -208,14 +204,11 @@ class CoordinateTree:
         }
         light_source { <-1, -1, -30> color White }
         """
-        return p;
+        return p
 
     def povray_cylinder(self, v1, v2, color):
-            v_str =\
-                'cylinder {{ <{0}, {1}, {2}>, <{3}, {4}, {5}>, 0.1 open texture {{ pigment {{ color {6}}}}}}}\n'\
-                .format(v1[0,0], v1[1,0], v1[2,0], v2[0,0], v2[1,0], v2[2,0], color)
+        v_str =\
+            'cylinder {{ <{0}, {1}, {2}>, <{3}, {4}, {5}>, 0.1 open texture {{ pigment {{ color {6}}}}}}}\n'\
+            .format(v1[0, 0], v1[1, 0], v1[2, 0], v2[0, 0], v2[1, 0], v2[2, 0], color)
 
-
-            return v_str
-
-
+        return v_str
